6451ad4647e2f279face9f754b73f90759ae5c12
卢 <2830767129@qq.com>
2024-01-12 15:11:52 +0800

    lyd

--- RoutePlanningSystem.hpp
+++ RoutePlanningSystem.hpp
@@ -81,7 +81,7 @@ public:
     
     // 从文件读数据建立城市交通库
     void createFromfile(const std::string& filepath) {
-        this->CreateGraphFromFile(filepath, g);
+        this->CreateGraphFromFile2(filepath, g);
     }
     //城市数
     void printcitynum() {
@@ -394,8 +394,9 @@ public:
            Weight dis = msf.get_index_distance(i);
            if (dis <= R) {
                std::cout<<msf[i]<<" ";
+               total++;
            }
-           total++;
+           
        }
        std::cout << "R内城市数：" << total << std::endl;
        //delete[]vertexs;
@@ -528,7 +529,8 @@ public:
 
    void MainMenu()
    {
-       while (1)
+       int trg = 1;
+       while (trg)
        {
            system("cls");
            
@@ -683,11 +685,11 @@ public:
                break;
            }
            case 23: {
-               this->createFromfile("usa.txt");
+               this->createFromfile("usa_small.txt");
                std::cout << "加载成功" << std::endl;
                break;
            }
-           case 24:exit(1); break;
+           case 24:trg = 0; break;
            default:
                cout << "Error Input" << endl;
                break;
